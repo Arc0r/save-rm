149b383933678aa1770fc66972dba21d401aa44d
Paul Müller <pmueller@physi.uni-heidelberg.de>
2024-06-13 09:42:40 +0200

    modified:   rm.sh clean function added

--- rm.sh
+++ rm.sh
@@ -2,40 +2,51 @@
 
 if [ -z "$1" ]; then
   # No arguments were passed, so print a help message
-  echo "Usage: todo.sh [command]"
+  echo "Usage: save_rm.sh [command|filename]"
   echo "Commands:"
-  echo "  list   List the contents of the todo file."
-  echo "  edit   Open the todo file in a text editor."
-  echo "  -h     Show this help message."
+  echo "  -l    List the deleted files."
+  echo "  -c    Clean the cached files."
+  echo "  -h    Show this help message."
   exit 1
 fi
 
 # Get the command from the first argument
 command="$1"
+CACHEFILE="/tmp/cache_saverm"
 
 # Check the command and take appropriate action
 case $command in
-  list)
-    cat ~/.cache/save_rm.cache
+  -l)
+    cat $CACHEFILE
+    exit 0
+    ;;
+  -c)
+    while read line
+    do 
+      rm $(echo $line|awk '{print $2}')
+    done < .cache/save_rm.cache
+    echo "Removed cached files"
     exit 0
     ;;
   -h)
-    echo "Usage: todo.sh [command]"
+    echo "Usage: save_rm.sh [command|filename]"
     echo "Commands:"
-    echo "  list   List the deleted files."
-    echo "  -h     Show this help message."
+    echo "  -l    List the deleted files."
+    echo "  -c    Clean the cached files."
+    echo "  -h    Show this help message."
     exit 0
     ;;
   *)
     STORE=$(mktemp -d)
     FILES=$@
     for FILE in $FILES
+      $FILE = readlink -f $FILE
       do
         if [[ -f "$FILE" || -d "$FILE" ]]; then
-          mv $FILE $STORE
+          mv $FILE $STORE/
           SIZE=$(du -sh $STORE)
           echo "Files are removed savely"
-          echo $SIZE $FILE   >> ~/.cache/save_rm.cache
+          echo $SIZE $FILE   >> $CACHEFILE
         fi
       done
     exit 0
